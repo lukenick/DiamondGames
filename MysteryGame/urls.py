13cb117e22f6dbeb5d706d9057adf519be4746c1
Luke Nick <lukenickholds@gmail.com>
2020-10-10 17:50:01 +1300

    Create reveal page, fix multiple clue bug
    Created the reveal page for display at the end of the game.
    Fixed a bug where multiple clues would get stacked onto the URL together
    and this not properly resolve

--- MysteryGame/urls.py
+++ MysteryGame/urls.py
@@ -9,8 +9,8 @@ urlpatterns = [
     path('host', views.host, name='host'),
     path('info', views.info, name='info'),
     path('band', views.band, name='band'),
+    path('reveal', views.reveal, name='reveal'),
     path('join?input_code=<str:character_id>', views.character, name='character'),
+    path('character/<str:character_id>-<str:clue_id>', views.character_with_clues, name='character_with_clues'),
     path('character/<str:character_id>', views.character, name='character'),
-    path('character/<str:character_id>/<str:clue_id>', views.character_with_clues, name='character_with_clues')
-
 ]
--- MysteryGame/views.py
+++ MysteryGame/views.py
@@ -17,6 +17,9 @@ def host(request):
 def info(request):
     return render(request, 'MysteryGame/info.html')
 
+def reveal(request):
+    return render(request, 'MysteryGame/reveal.html')
+
 def band(request):
     global_character = BandCharacter.objects.get(pk=10)
     all_characters = BandCharacter.objects.order_by('pk')
@@ -25,10 +28,15 @@ def band(request):
 
 
 def character(request, character_id):
+    if character_id=="TheBigReveal":                            # If they input the end game code, redirect to the REVEAL page
+        return render(request, 'MysteryGame/reveal.html')
+
     this_character = BandCharacter.objects.get(code=character_id)
     global_character = BandCharacter.objects.get(pk=10)
     all_characters = BandCharacter.objects.order_by('pk')
     return render(request, 'MysteryGame/character.html', {'this_character': this_character, 'global_character': global_character, 'all_characters': all_characters})
+
+
 
 def character_with_clues(request, character_id, clue_id):
     this_character = BandCharacter.objects.get(code=character_id)
